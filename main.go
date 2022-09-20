0699b4460a1920fe5d2db06f83dbe1a6914e88ee
Ramish Hassan <ramishshah93@gmail.com>
2022-09-20 13:35:29 +0200

    board in cli done with current values for each of the positions

--- main.go
+++ main.go
@@ -1,3 +1,36 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+
+type ticktackBorad [3][3]rune
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	
+	var playerMove bool
+	var whoWon string
+	var win bool
+
+	var board ticktackBorad
+
+	fmt.Println("Starting Game Board empty")
+	board.displayBoard()
+	fmt.Println("*****%v won *** final board view,", whoWon)
+}
+
+func (t ticktackBorad) displayBoard(){
+	fmt.Print("-------------")
+	for i := 0; i < 3; i++{
+		fmt.Print("\n|")
+		for j := 0; j < 3; j++{
+			fmt.Printf("%c |", t[i][j])
+		}
+		fmt.Print("\n ----------")
+	}
+	fmt.Print("\n")
+}
